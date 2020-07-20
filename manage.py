185cc40a3fc185e74382f4912ad1fb51201cf91e
edumorris <eduuwachira@gmail.com>
2020-07-21 00:14:28 +0300

    Application manage file initialized

---
+++ manage.py
@@ -0,0 +1,24 @@
+from app import create_app
+from flask_script import Manager, Server
+# from app.models import models
+
+# App instance 
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+@manager.command
+def test():
+       '''
+       Run the unit test
+       '''
+       import unittest
+       tests = unittest.TestLoader().discover('tests')
+       unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+       return dict(app = app)
+
+if __name__ == '__main__':
+    manager.run()
