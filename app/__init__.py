2862d6557e4b45f4be45bb37ba55f8736a559c56
edumorris <eduuwachira@gmail.com>
2020-07-21 00:10:19 +0300

    Application __init__.py values definition

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # App config
+    app.config.from_object(config_options[config_name])
+
+    # Extensions init
+    bootstrap.init_app(app)
+
+    # Main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Request config
+    '''
+    from .requests import configure_request
+    configure_request(app)
+    '''
+
+    return app
